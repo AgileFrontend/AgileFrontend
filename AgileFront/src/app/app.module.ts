4cca4ce9860b1988f5b50729f9e63caa7f509a74
Guillaume Ruff <guiguiruff@gmail.com>
2023-11-05 01:12:57 +0100

    Initial commit firebase + material added

---
+++ AgileFront/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp({"projectId":"agilebackend-9e4d6","appId":"1:592568335590:web:b4712eaa69ff0502cccf27","storageBucket":"agilebackend-9e4d6.appspot.com","apiKey":"AIzaSyCEoR-Q079wqtiXOaMG9jXUdxtclKHGqmQ","authDomain":"agilebackend-9e4d6.firebaseapp.com","messagingSenderId":"592568335590","measurementId":"G-5T240TZMVG"})),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage()),
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ AgileFront/src/environments/environment.ts
@@ -0,0 +1,20 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyCEoR-Q079wqtiXOaMG9jXUdxtclKHGqmQ",
+
+        authDomain: "agilebackend-9e4d6.firebaseapp.com",
+      
+        projectId: "agilebackend-9e4d6",
+      
+        storageBucket: "agilebackend-9e4d6.appspot.com",
+      
+        messagingSenderId: "592568335590",
+      
+        appId: "1:592568335590:web:b4712eaa69ff0502cccf27",
+      
+        measurementId: "G-5T240TZMVG"
+      
+    }
+  };
+  
