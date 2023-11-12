13f2646b7e3abfd2b1b36235f0bb34b204c6f455
Saad Moutaouakil <saad.moutaouakil@insa-rennes.fr>
2023-11-12 22:02:07 +0000

    Fixed routing between completed pages

--- AgileFront/src/app/app-routing.module.ts
+++ AgileFront/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { CreatePostComponent } from './create.post/create.post.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent }, // route to the login page
   { path: 'register', component: RegisterComponent },
   { path: 'create.post', component: CreatePostComponent },
+  { path: 'homepage', component: HomepageComponent}
 ];
 
 @NgModule({
--- AgileFront/src/app/navbar/navbar.component.ts
+++ AgileFront/src/app/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -10,4 +11,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   standalone: true,
   imports: [MatToolbarModule, MatButtonModule, MatIconModule],
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+
+  constructor(protected router : Router) {
+  }
+}
