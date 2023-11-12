a924cb4c625c7666d7a38bae2ab613ff47ca6c10
CROUTS DE PAILLE Benjamin <bencdep@gmail.com>
2023-11-12 20:27:43 +0100

    Add Auth Guard and AuthService

--- AgileFront/src/app/app-routing.module.ts
+++ AgileFront/src/app/app-routing.module.ts
@@ -5,13 +5,14 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { MyProjectPageComponent } from './my-project-page/my-project-page.component';
+import { authGuard } from './services/auth/auth.service';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent }, // route to the login page
   { path: 'register', component: RegisterComponent },
-  { path: 'create.post', component: CreatePostComponent },
   { path: 'homepage', component: HomepageComponent},
   { path: 'my-project-page', component: MyProjectPageComponent}
+  { path: 'create.post', component: CreatePostComponent, canActivate: [authGuard] }
 ];
 
 @NgModule({
