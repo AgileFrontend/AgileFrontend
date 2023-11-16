8d1654e37a7fe57aed284e64084ddc2a437c3091
Guillaume Ruff <guiguiruff@gmail.com>
2023-11-16 10:02:13 +0000

    Fixed loop routing to login

--- AgileFront/src/app/app-routing.module.ts
+++ AgileFront/src/app/app-routing.module.ts
@@ -3,12 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { CreatePostComponent } from './create.post/create.post.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { authGuard } from './services/auth/auth.service';
 import { HomepageComponent } from './homepage/homepage.component';
 import { MyProjectPageComponent } from './my-project-page/my-project-page.component';
+import { authGuard } from './services/auth/auth.service';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent, canActivate: [authGuard] }, // route to the login page
+  
   { path: 'register', component: RegisterComponent },
   { path: 'homepage', component: HomepageComponent },
   {
@@ -16,6 +16,7 @@ const routes: Routes = [
     component: MyProjectPageComponent,
     canActivate: [authGuard],
   },
+  { path: 'login', component: LoginComponent}, // route to the login page
   {
     path: 'create.post',
     component: CreatePostComponent,
--- AgileFront/src/app/services/auth/auth.service.ts
+++ AgileFront/src/app/services/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import { Injectable, inject } from '@angular/core';
 import { User } from '../user';
 import { Auth } from '@angular/fire/auth';
-import { CanActivateFn, Router } from '@angular/router';
+import { CanActivateFn, Router, UrlTree } from '@angular/router';
+import { throwDialogContentAlreadyAttachedError } from '@angular/cdk/dialog';
 
 @Injectable({
   providedIn: 'root',
@@ -31,20 +32,36 @@ export class AuthService {
         if (user) {
           resolve(true);
         } else {
-          this.router.navigate(['login']);
           resolve(false);
         }
       });
     });
   }
 
+  authGuardFn(): Promise<boolean> {
+    return new Promise((resolve) => {
+      this.auth.onAuthStateChanged((user) => {
+        if (user) {
+          resolve(true);
+        } else {
+          this.router.navigate(['login'])
+          resolve(false);
+        }
+      });
+    });
+  }
+
+  
+
   //Provide a way to log out the currently logged in user
   logOut() {
     this.auth.signOut();
   }
+  
+  
 }
 
 // Provide a way to prevent any anonymous user to access user-tied pages via the canActivate parameter in Routes
 export const authGuard: CanActivateFn = () => {
-  return inject(AuthService).isLoggedIn();
+  return inject(AuthService).authGuardFn();
 };
