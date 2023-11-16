c00a3f2ae4c5c8be43f45ced22b49d39b471b083
Guillaume Ruff <guiguiruff@gmail.com>
2023-11-16 10:02:25 +0000

    trying to rewrite the isLoggedIn part of nav-bar

--- AgileFront/src/app/navbar/navbar.component.ts
+++ AgileFront/src/app/navbar/navbar.component.ts
@@ -1,17 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth/auth.service';
-
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {
-  isLogged = false;
+export class NavbarComponent implements OnInit {
+  isLoggedIn = false
+  constructor(private auth: AuthService) {}
 
-  constructor(private auth: AuthService) {
-    this.auth.isLoggedIn().then((loggedIn) => {
-      this.isLogged = loggedIn;
-    });
+  ngOnInit(): void {
+    this.checkLogState()
   }
-}
+
+  checkLogState(){
+    this.auth.isLoggedIn().then((logState) =>{
+      this.isLoggedIn = logState
+    })
+    return this.isLoggedIn
+  }
+} 
