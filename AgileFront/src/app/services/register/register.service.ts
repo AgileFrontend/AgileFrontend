8fa1aed74f4ebee5f03621bf042816b87d6dc5e5
CROUTS DE PAILLE Benjamin <bencdep@gmail.com>
2023-11-16 20:50:36 +0000

    Create UserData after register

--- AgileFront/src/app/services/register/register.service.ts
+++ AgileFront/src/app/services/register/register.service.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Auth, User } from '@angular/fire/auth';
 import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import { User as UserFirestore } from '../user';
+import {
+  Firestore,
+  setDoc,
+  doc
+} from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RegisterService {
-  constructor(private auth: Auth) {
+  constructor(private auth: Auth, private store: Firestore,) {
     this.auth = getAuth();
   }
 
@@ -22,6 +28,19 @@ export class RegisterService {
     return new Promise((resolve, reject) => {
       try {
         const user = result.user;
+        const userData : UserFirestore = {
+          email: user.email == null ? '' : user.email,
+          name: '',
+          surname: '',
+          phoneNumber: '',
+          bio: '',
+          photoURL: '',
+          address: '',
+          town: '',
+          postalCode: ''
+        }
+        this.createUser(userData,user.uid)
+        .catch((err) => console.error(err))
         resolve(user);
       } catch (error) {
         if (error) {
@@ -30,4 +49,11 @@ export class RegisterService {
       }
     });
   }
+
+  async createUser(user: UserFirestore, uid: string) {
+    const docRef = doc(this.store, 'users', uid)
+    const result = await setDoc(docRef, user, { merge: true })
+    return result
+  }
+
 }
