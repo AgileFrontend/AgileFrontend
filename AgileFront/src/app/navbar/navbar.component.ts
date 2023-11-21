df7e804d9e04059b40085fea0e47741b0ff4345f
Saad Moutaouakil <saad.moutaouakil@insa-rennes.fr>
2023-11-21 18:54:14 +0000

    switched the whole navbar to angular materials tabs

--- AgileFront/src/app/navbar/navbar.component.ts
+++ AgileFront/src/app/navbar/navbar.component.ts
@@ -1,10 +1,13 @@
 import { Component, NgZone } from '@angular/core';
 import { Auth } from '@angular/fire/auth';
 import { AuthService } from '../services/auth/auth.service';
+import { MatTabChangeEvent } from '@angular/material/tabs';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss'],
+  styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
   isLoggedIn = false;
@@ -12,6 +15,7 @@ export class NavbarComponent {
     private afAuth: Auth,
     private zone: NgZone,
     private auth: AuthService,
+    private router : Router
   ) {
     this.checkUserStatus();
   }
@@ -30,5 +34,25 @@ export class NavbarComponent {
   logOut() {
     this.auth.logOut();
     this.isLoggedIn = false;
+  }
+
+
+  onTabChanged(event : MatTabChangeEvent) {
+    switch(event.index) {
+      case 0 : 
+        this.router.navigate(['homepage']);
+        break;
+      case 1 : 
+        break;
+      case 2 :
+        this.router.navigate(['my-project-page']);
+        break;
+      case 3 : 
+        this.router.navigate(['profile/me/']);
+        break;
+      case 4 : 
+        this.logOut();
+        break;
+    }
   }
 }
